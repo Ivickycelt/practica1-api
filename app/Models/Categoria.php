4877b46a22d0dccc5c6d68fd57c5c2c642140ffd
Luis Fernando Gomez Colin <postdanger61@gmail.com>
2026-06-02 22:50:58 -0600

    correccion de codigo vacio

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'slug', 'descripcion'];
+
+    // Esta es la relación inversa a la que definimos en Producto.php
+    public function productos(): HasMany
+    {
+        return $this->hasMany(Producto::class); //
+    }
 }
